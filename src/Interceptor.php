d7d4a17b6c81f798b89b54afaafc382ae12ed4ab
Gabriel Ostrolucký <gabriel.ostrolucky@gmail.com>
2022-01-18 16:10:12 +0100

    Allow optional interceptors

--- src/Interceptor.php
+++ src/Interceptor.php
@@ -11,9 +11,9 @@ use function function_exists;
 
 class Interceptor
 {
-    private RedisCallInterceptor $wrappedInterceptor;
+    private $wrappedInterceptor;
 
-    public function __construct(RedisCallInterceptor $wrappedInterceptor)
+    public function __construct(callable $wrappedInterceptor)
     {
         $this->wrappedInterceptor = $wrappedInterceptor;
     }
@@ -27,11 +27,11 @@ class Interceptor
     {
         if (!function_exists('newrelic_record_datastore_segment') || !$instance instanceof RedisCluster) {
             // Nothing to do here. NR PHP extension supports \Redis and Predis. Only thing it doesn't support is \RedisCluster
-            return $this->wrappedInterceptor->__invoke($instance, $method, $args, $connection);
+            return ($this->wrappedInterceptor)($instance, $method, $args, $connection);
         }
 
         return newrelic_record_datastore_segment(
-            fn () => $this->wrappedInterceptor->__invoke($instance, $method, $args, $connection),
+            fn () => ($this->wrappedInterceptor)($instance, $method, $args, $connection),
             ['product' => 'Redis', 'operation' => $method],
         );
     }
